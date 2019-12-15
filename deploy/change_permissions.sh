9fb8ef086a0794d1a315f4a9756382452e4f0e90
Fernando Pires <alariofernandopires@gmail.com>
2019-12-15 13:30:43 -0300

    Force Deployment

--- deploy/change_permissions.sh
+++ deploy/change_permissions.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-chmod 775 /var/www/FlaskAPI
+ls /home/ec2-user
+cd /home/ec2-user/FlaskAPI
+pip3 install -r requirements.txt
--- deploy/install_dependencies.sh
+++ deploy/install_dependencies.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 sudo yum install python3 -y
-cd /home/ec2-user/FlaskAPI
-pip3 install -r requirements.txt
