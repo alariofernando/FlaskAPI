d2b92340f1b6cf6faf48a9c3caa00bd77a4871b6
Fernando Pires <alariofernandopires@gmail.com>
2019-12-15 12:56:02 -0300

    Add /

--- APP/app.py
+++ APP/app.py
@@ -4,9 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/test')
+@app.route('/hello')
 def helloworld():
     return "Hello World"
+
+@app.route('/')
+def index():
+    return "Try /hello"
 
 if __name__ == "__main__":
     app.run(port=5000)
